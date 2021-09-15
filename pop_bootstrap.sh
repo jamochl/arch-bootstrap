c0d94fa9ba4789864af825c7d62fa8e20a1f9765
jamochl <james.lim@jamochl.com>
2021-09-16 00:21:37 +1000

    added simple pop_bootstrap.sh for testing

---
+++ pop_bootstrap.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -ex
+
+# Bootstrap Pop!_OS Linux
+
+# Environment setup
+local PACKAGE_DIR="$HOME/.config/packagelists"
+
+# Initial System Setup
+sudo apt update && sudo apt upgrade -y
+
+# Setup GIT
+sudo apt install -y git
+
+git config --global user.name "jamochl"
+git config --global user.email "james.lim@jamochl.com"
+git config --global user.useConfigOnly "true"
+
+# Setup dotfiles
+git clone --bare https://github.com/jamochl/dotfiles ~/.dotfiles
+git --work-tree="$HOME" --git-dir="$HOME/.dotfiles" config status.showUntrackedFiles no
+git --work-tree="$HOME" --git-dir="$HOME/.dotfiles" checkout --force master
+
+# Setup APT
+cat <<EOF | sudo tee /etc/apt/apt.conf
+APT::Install-Recommends "False";
+APT::Cache::ShowRecommends "True";
+EOF
+
+# Install dependencies
+sudo apt install -y $(cat $PACKAGE_DIR/{common_desired,ubuntu_desired}.list | grep '^\w')
+
+## Setup Flatpak
+
+
+# Service setup
+sudo systemctl disable NetworkManager-wait-online.service
+
+# Kernel Setup (Metabox Only)
+if [[ -n "$(lscpu -J | jq '.lscpu[] | select(.field == "Model name:") | .data')" ]]; then
+    sudo kernelstub -a "intel_idle.max_cstate=4"
+fi
