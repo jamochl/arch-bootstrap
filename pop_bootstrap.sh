6c426d124a54f3c23f96d504c519093b4a5528c8
jamochl <james.lim@jamochl.com>
2021-09-18 15:53:28 +1000

    add firewalld setup for virtualisation, as well as disabling apt-daily upgrades and scans

--- pop_bootstrap.sh
+++ pop_bootstrap.sh
@@ -26,11 +26,34 @@ APT::Install-Recommends "False";
 APT::Cache::ShowRecommends "True";
 EOF
 
+sudo systemctl disable apt-daily-upgrade.timer apt-daily.timer
+sudo systemctl mask apt-daily-upgrade apt-daily
+
+
 # Install dependencies
 sudo apt install -y $(cat $PACKAGE_DIR/{common_desired,ubuntu_desired}.list | grep '^\w')
 
-## Setup Flatpak
+## Setup Flatpak (TODO)
+## Setup aws-cli
 
+# Setup Firewalld
+sudo firewall-cmd --set-default-zone=home
+
+# Setup Virtualisation
+
+## User setup
+usermod $USER -aG kvm,libvirt
+
+## libvirt Firewalld
+sudo firewall-cmd --new-zone=libvirt --permanent
+sudo firewall-cmd --zone=libvirt --add-service={ssh,dhcpv6,dhcp,http,https,dns,tftp} --permanent
+sudo firewall-cmd --zone=libvirt --add-protocol={icmp,ipv6-icmp} --permanent
+sudo firewall-cmd --zone=libvirt --set-target=ACCEPT --permanent
+sudo firewall-cmd --zone=libvirt --add-rich-rule='rule priority="32767" reject' --permanent
+sudo firewall-cmd --reload
+
+sudo systemctl enable libvirtd
+sudo systemctl restart libvirtd
 
 # Service setup
 sudo systemctl disable NetworkManager-wait-online.service
