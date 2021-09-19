305cc601ded9a6bee9ef64f33745eebf70dc804d
jamochl <james.lim@jamochl.com>
2021-09-19 19:52:25 +1000

    add arch_bootstrap.sh which pulls arch specific functions

---
+++ arch_bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex
+
+# Bootstrap Pop!_OS Linux
+source functions/common.sh
+source functions/arch.sh
+
+# Environment setup
+PACKAGE_DIR="$PWD/pkglists"
+
+fn_home_setup
+fn_package_manager_setup
+fn_install_pkglists
+fn_git_setup
+fn_clone_dotfiles
+fn_user_setup
+fn_service_setup
+fn_utility_setup
+fn_kernel_setup
